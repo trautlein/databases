e0e039ffb4eef3f9145e50311fe5e5844bef1d54
Hans Trautlein <hans@trautlein.com>
2016-10-07 15:55:04 -0700

    Prevented problem of the first post appearing twice

--- client/refactor.js
+++ client/refactor.js
@@ -96,7 +96,7 @@ app = {
       contentType: 'application/json',
       success: function(json) {
         message.objectId = json.objectId;
-        app.displayMessage(message);
+        // app.displayMessage(message);
       },
       complete: function() {
         app.stopSpinner();
